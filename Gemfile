f598d22cc706f8d7971d61bbfb6cc032afedc272
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-05-24 15:52:59 +0000

    add simple calendar gem and views

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.1"
 
+# Simple calendar gem [https://github.com/excid3/simple_calendar]
+gem "simple_calendar"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 
--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,6 +1,11 @@
 class DaysController < ApplicationController
   def show
-    
-    render({ :template => "/days/show"})
+    Time.zone = "Pacific Time (US & Canada)"
+    @current_local_time = Time.zone.now
+
+    @start_time = @current_local_time.change(hour: 7)
+    @end_time = @current_local_time.change(hour: 23)
+
+    render({ :template => "/days/show" })
   end
 end
